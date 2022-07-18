e74b03cc52c182dac4ef5902710ad1f6ff38fab0
Andrew Erofeev <eroha210@mail.ru>
2022-07-19 00:13:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+            Console.WriteLine("{1}",input, input[1]);
+            break;
+    }
